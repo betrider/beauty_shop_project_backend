0ef391004e7adb693479d3fd3ca0b1a8f851c8f9
siu9068 <siu9068@gmail.com>
2020-07-02 22:37:34 +0900

    -CORS 이슈 수정

--- src/main/java/com/shop/beauty/config/WebConfig.java
+++ src/main/java/com/shop/beauty/config/WebConfig.java
@@ -9,6 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/api/**").allowedOrigins("*");
     }
 }
